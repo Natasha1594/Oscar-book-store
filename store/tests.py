ec42a6cd71af302b3a7383a0483bdabebf20acab
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-10 22:20:05 +0300

    update

--- store/tests.py
+++ store/tests.py
@@ -17,9 +17,4 @@ class CategoryTestClass(TestCase):
     def test_delete_category(self):
         self.category.delete_category()
         category = Category.objects.all()
-        self.assertTrue(len(category) == 0
-	#name = models.CharField(max_length = 100)
-	#slug = models.SlugField(max_length = 150, unique=True ,db_index=True)
-	#icon = models.FileField(upload_to = "category/")
-	#create_at = models.DateTimeField(auto_now_add = True)
-	#updated_at = models.DateTimeField(auto_now_add = True)
+        self.assertTrue(len(category) == 0)
